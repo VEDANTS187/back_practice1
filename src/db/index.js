2a3844a2b1cd17401b1b777fd13d02575d4b49e8
VEDANTS187 <vedantashirke18@gmail.com>
2025-04-29 11:13:53 +0530

    practice

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const CONNECTDB = async()=>{
+
+    try {
+      const connectioninstance = await mongoose.connect(`${process.env.DB_URL}/${DB_NAME}`)
+        console.log("MONGODB CONNECTED !!",connectioninstance.connection.host)
+    } catch (error) {
+        console.error("MONGODB connection error",error);
+    }
+}
+
+export default CONNECTDB;
